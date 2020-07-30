522365b0810e389d3ca036330ad5378da1c9b711
Shoyo Inokuchi <shoyoinokuchi@gmail.com>
2020-07-30 19:58:27 +0900

    Extract minikv as trait not struct

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,28 @@ use std::hash::Hash;
 
 type Result<T> = std::result::Result<Option<T>, Box<dyn std::error::Error>>;
 
+trait MiniKV<K, V> {
+    fn new() -> Self;
+    fn set(&mut self, key: K, val: V) -> Result<V>;
+    fn get(&self, key: &K) -> Result<&V>;
+}
+
 #[derive(Debug)]
-struct MiniKV<K, V> {
+struct HashMiniKV<K, V> {
     store: HashMap<K, V>,
 }
 
-impl<K, V> MiniKV<K, V>
+impl<K, V> MiniKV<K, V> for HashMiniKV<K, V>
 where
     K: Eq + PartialEq + Hash,
 {
-    pub fn new() -> Self {
-        MiniKV {
+    fn new() -> Self {
+        HashMiniKV {
             store: HashMap::new(),
         }
     }
 
-    pub fn set(&mut self, key: K, val: V) -> Result<V> {
+    fn set(&mut self, key: K, val: V) -> Result<V> {
         // If some constraints are not fulfilled, return an error
         match self.store.insert(key, val) {
             Some(val) => Ok(Some(val)),
@@ -28,7 +34,7 @@ where
         }
     }
 
-    pub fn get(&self, key: &K) -> Result<&V> {
+    fn get(&self, key: &K) -> Result<&V> {
         // If some constraints are not fulfilled, return an error
         match self.store.get(&key) {
             Some(val) => Ok(Some(val)),
@@ -51,8 +57,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let mut store: MiniKV<String, u32> = MiniKV::new();
+    fn test_hash_minikv() {
+        let mut store: HashMiniKV<String, u32> = MiniKV::new();
         store.set("foo".to_string(), 5);
         assert_eq!(store.get(&"foo".to_string()).unwrap(), Some(&5));
         assert_eq!(store.get(&"bar".to_string()).unwrap(), None);
