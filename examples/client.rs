18a67cdcf9849ce27c3cf663de827775b87c82cf
Shoyo Inokuchi <shoyoinokuchi@gmail.com>
2020-07-31 16:32:03 +0900

    Bugfix client-side buffer issue, start integrating async

--- examples/client.rs
+++ examples/client.rs
@@ -1,13 +1,15 @@
 use std::io::prelude::*;
-use std::net::TcpStream;
+use tokio::net::TcpStream;
+use tokio::prelude::*;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("====================");
     println!("MiniKV Client (v0.1)");
     println!("====================");
 
     let url = "127.0.0.1:6464";
-    let mut stream = TcpStream::connect(url).unwrap();
+    let mut stream = TcpStream::connect(url).await?;
 
     println!("** Successfully established outbound TCP connection");
     println!("** Listening on: {}", url);
@@ -19,10 +21,10 @@ fn main() {
         std::io::stdin()
             .read_line(&mut wbuf)
             .expect("Failed to read input");
-        let _ = stream.write(wbuf.as_bytes());
+        stream.write_all(wbuf.as_bytes()).await?;
 
-        let mut rbuf = [0, 128];
-        let _ = stream.read(&mut rbuf);
+        let mut rbuf = [0; 128];
+        stream.read(&mut rbuf[..]).await?;
 
         println!("{}", String::from_utf8_lossy(&rbuf));
     }
