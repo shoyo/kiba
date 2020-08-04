dc8d29f8b1b584bd25c0969b7f9b595f4b74f574
Shoyo Inokuchi <shoyoinokuchi@gmail.com>
2020-08-05 00:34:31 +0900

    Add documentation for Store trait

--- src/store.rs
+++ src/store.rs
@@ -1,46 +1,186 @@
 use std::cmp::Eq;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fmt;
 use std::hash::Hash;
 
-type Result<T> = std::result::Result<Option<T>, Box<dyn std::error::Error>>;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-pub trait Store<K, V> {
+/// A shared interface for implementations of store.
+/// Time and space complexities of each function are not guaranteed and
+/// depends on each implementation. (hash vs. btree, vec vs. linked list etc.)
+trait Store {
+    /// Create a new store.
     fn new() -> Self;
-    fn set(&mut self, key: K, val: V) -> Result<V>;
-    fn get(&self, key: &K) -> Result<&V>;
+
+    // Strings Operations
+
+    /// Get the value of a key.
+    /// If the key does not exist, return None.
+    fn get(&self, key: &str) -> Result<&str>;
+
+    /// Set the value of a key.
+    /// If the key already existed, return previous value.
+    /// Otherwise, return None.
+    fn set(&mut self, key: &str, val: &str) -> Result<Option<&str>>;
+
+    /// Increment the value of a key by 1.
+    /// Return the updated value.
+    /// If the key does not exist, return an error (unlike Redis).
+    /// If the value is not/cannot be interpreted as an integer, return an error.
+    /// This operation is limited to 64-bit integers.
+    fn incr(&mut self, key: &str) -> Result<i64>;
+
+    /// Decrement the value of a key by 1.
+    /// Return the updated value.
+    /// If the key does not exist, return an error (unlike Redis).
+    /// If the value is not/cannot be interpreted as an integer, return an error.
+    /// This operation is limited to 64-bit integers.
+    fn decr(&mut self, key: &str) -> Result<i64>;
+
+    /// Increment the value of a key by a specified amount.
+    /// Return the updated value.
+    /// If the key does not exist, return an error (unlike Redis).
+    /// If the value is not/cannot beinterpreted as an integer, return an error.
+    /// This operation is limited to 64-bit integers.
+    /// Time complexity: O(1)
+    fn incrby(&mut self, key: &str, by: i64) -> Result<i64>;
+
+    /// Decrement the value of a key by a specifed amount.
+    /// Return the updated value.
+    /// If the key does not exist, return an error (unlike Redis).
+    /// If the value is not/cannot beinterpreted as an integer, return an error.
+    /// This operation is limited to 64-bit integers.
+    fn decrby(&mut self, key: &str, by: i64) -> Result<i64>;
+
+    // Lists Operations
+
+    /// Insert value at the head of list stored at key.
+    /// Return the updated length of the list.
+    /// If the key does not exist, create an empty list before performing the operation.
+    fn lpush(&mut self, key: &str, val: &str) -> Result<u64>;
+
+    /// Insert value at the tail of list stored at key.
+    /// Return the updated length of the list.
+    /// If the key does not exist, create an empty list before performing the operation.
+    fn rpush(&mut self, key: &str, val: &str) -> Result<u64>;
+
+    /// Remove and return the element at the head of list stored at key.
+    /// If the key does not exist, return an error.
+    fn lpop(&mut self, key: &str, val: &str) -> Result<u64>;
+
+    /// Remove and return the element at the head of list stored at key.
+    /// If the key does not exist, return an error.
+    fn rpop(&mut self, key: &str, val: &str) -> Result<u64>;
+
+    // Sets Operations
+
+    /// Insert value in the set stored at key.
+    /// Return the updated length of the set.
+    /// If the key does not exist, create an empty set before performing the operation.
+    fn sadd(&mut self, key: &str, val: &str) -> Result<u64>;
+
+    /// Remove value in the set stored at key.
+    /// Return the updated length of the set.
+    /// If the key does not exist, create an empty set before performing the operation.
+    fn srem(&mut self, key: &str, val: &str) -> Result<u64>;
+
+    /// Return if value is a member of the set stored at key.
+    fn sismember(&self, key: &str, val: &str) -> Result<bool>;
+
+    /// Return all members of the set stored at key.
+    fn smembers(&self, key: &str) -> Result<Vec<&str>>;
+
+    // Hashes Operations
+
+    /// Get the value related to field in the hash stored at key.
+    /// If the field does not exist, return None.
+    /// If the key does not exist, return an error.
+    fn hget(&self, key: &str, field: &str) -> Result<&str>;
+
+    /// Set the field of the hash stored at key to value.
+    /// If the field already existed, return previous value.
+    /// Otherwise, return None.
+    /// If the key does not exist, create an empty hash before performing the operation.
+    fn hset(&mut self, key: &str, field: &str, val: &str) -> Result<Option<&str>>;
+
+    /// Remove field from the hash stored at key.
+    /// Return the number of fields that were deleted.
+    /// If the field does not exist, do nothing (and return 0).
+    /// If the key does not exist, return an error.
+    fn hdel(&mut self, key: &str, field: &str) -> Result<u64>;
 }
 
 #[derive(Debug)]
-pub struct HashStore<K, V> {
-    store: HashMap<K, V>,
+pub struct StdStore {
+    strings: HashMap<String, String>,
+    lists: HashMap<String, Vec<String>>,
+    hashes: HashMap<String, HashMap<String, String>>,
+    sets: HashMap<String, HashSet<String, String>>,
 }
 
-impl<K, V> Store<K, V> for HashStore<K, V>
-where
-    K: Eq + PartialEq + Hash,
-{
+impl Store for StdStore {
     fn new() -> Self {
-        HashStore {
-            store: HashMap::new(),
+        Store {
+            strings: HashMap::new(),
+            lists: HashMap::new(),
+            hashes: HashMap::new(),
+            sets: HashMap::new(),
         }
     }
 
-    fn get(&self, key: &K) -> Result<&V> {
-        // If some constraints are not fulfilled, return an error
-        match self.store.get(&key) {
+    // Strings Operations
+
+    pub fn get(&self, key: &str) -> Result<Option<&str>> {
+        match self.strings.get(&key) {
             Some(val) => Ok(Some(val)),
             None => Ok(None),
         }
     }
 
-    fn set(&mut self, key: K, val: V) -> Result<V> {
-        // If some constraints are not fulfilled, return an error
-        match self.store.insert(key, val) {
+    pub fn set(&mut self, key: &str, val: &str) -> Result<Option<&str>> {
+        // If some contraints are not satisfied (e.g. out of memory)
+        // return an error
+        match self.strings.insert(key, val) {
             Some(val) => Ok(Some(val)),
             None => Ok(None),
         }
     }
+
+    pub fn incr(&mut self, key: &str) -> Result<i64> {}
+
+    pub fn decr(&mut self, key: &str) -> Result<i64> {}
+
+    pub fn incrby(&mut self, key: &str, by: i64) -> Result<i64> {}
+
+    pub fn decrby(&mut self, key: &str, by: i64) -> Result<i64> {}
+
+    /// Lists Operations
+
+    pub fn lpush(&mut self, key: &str, val: &str) -> Result<u64> {}
+
+    pub fn rpush(&mut self, key: &str, val: &str) -> Result<u64> {}
+
+    pub fn lpop(&mut self, key: &str, val: &str) -> Result<u64> {}
+
+    pub fn rpop(&mut self, key: &str, val: &str) -> Result<u64> {}
+
+    /// Sets Operations
+
+    pub fn sadd(&mut self, key: &str, val: &str) -> Result<u64> {}
+
+    pub fn srem(&mut self, key: &str, val: &str) -> Result<u64> {}
+
+    pub fn sismember(&self, key: &str, val: &str) -> Result<bool> {}
+
+    pub fn smembers(&self, key: &str) -> Result<Vec<&str>> {}
+
+    /// Hashes Operations
+
+    pub fn hset(&mut self, key: &str, hkey: &str, hval: &str) -> Result<Option<&str>> {}
+
+    pub fn hget(&self, key: &str, hkey: &str) -> Result<&str> {}
+
+    pub fn hdel(&mut self, key: &str, hkey: &str) -> Result<()> {}
 }
 
 #[derive(Debug, Clone)]
@@ -57,12 +197,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_hash_store() {
-        let mut store: HashStore<String, u32> = Store::new();
-        let _ = store.set("foo".to_string(), 5);
-        assert_eq!(store.get(&"foo".to_string()).unwrap(), Some(&5));
-        assert_eq!(store.get(&"bar".to_string()).unwrap(), None);
-        assert_eq!(store.set("baz".to_string(), 7).unwrap(), None);
-        assert_eq!(store.set("foo".to_string(), 8).unwrap(), Some(5));
+    fn it_works() {
+        assert_eq!(true, true);
     }
 }
