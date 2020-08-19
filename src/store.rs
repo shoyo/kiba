02e6f98d2d87dd8248afa90c3ee8a83c564cdddd
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2020-08-19 17:05:00 +0900

    Add trait declarations for upcoming commands

--- src/store.rs
+++ src/store.rs
@@ -4,30 +4,57 @@ type Result<T> = std::result::Result<T, OperationalError>;
 
 /// A shared interface for implementations of store.
 /// Time and space complexities of each function are not guaranteed and
-/// depends on each implementation. (hash vs. btree, vec vs. linked list etc.)
+/// depends on the implementation. (hash vs. btree, vec vs. linked list etc.)
 pub trait Store {
     /// Create a new store.
     fn new() -> Self;
 
+    /// Delete the value stored at key. Value can be any data type.
+    /// If the delete was successful, return the deleted key.
+    /// If the key doesn't exist, return None.
+    fn del(&mut self, key: String) -> Result<Option<String>>;
+
+    /// Delete all keys in the store.
+    fn flushdb(&mut self) -> Result<()>;
+
     // Strings Operations
 
-    /// Get the value of a key.
+    /// Get the string stored at key.
     /// If the key does not exist, return None.
-    /// Time complexity: O(1)
+    /// If the key does not hold a string, return an error.
     fn get(&self, key: String) -> Result<Option<String>>;
 
     /// Set the value of a key.
     /// If the key already existed, return previous value.
-    /// Otherwise, return None.
-    /// Time complexity: O(1)
+    /// If the key did not already exist, return None.
+    /// If the key does not hold a string, return an error.
     fn set(&mut self, key: String, val: String) -> Result<Option<String>>;
+
+    /// Concatenate value to the string stored at key and return the result.
+    /// If the key does not exist, initialize an empty string then concatenate.
+    /// If the key does not hold a string, return an error.
+    fn append(&mut self, key: String, val: String) -> Result<String>;
+
+    /// Return the substring of the string stored at key (zero-based index).
+    /// Negative indices refer to the index from the end of the string.
+    /// (ex. -1 refers to last index, -2 refers to second-to-last index, etc.)
+    /// Out-of-bounds indices do not return an error. Instead, the range is confined
+    /// to the length of the string.
+    /// If the key does not exist, return None.
+    /// If the key does not hold a string, return an error.
+    fn getrange(&self, key: String, start: i64, end: i64) -> Result<Option<String>>;
+
+    /// Return the length of the string stored at key.
+    /// If the key does not exist, return 0.
+    /// If the key does not hold a string, return an error.
+    fn strlen(&self, key: String) -> Result<u64>;
 
     /// Increment the value of a key by 1.
     /// Return the updated value.
     /// If the key does not exist, return an error (unlike Redis).
     /// If the value is not/cannot be interpreted as an integer, return an error.
     /// This operation is limited to 64-bit integers.
-    /// Time complexity: O(1)
+    /// If the key does not hold a string, return an error.
     fn incr(&mut self, key: String) -> Result<i64>;
 
     /// Decrement the value of a key by 1.
@@ -35,7 +62,7 @@ pub trait Store {
     /// If the key does not exist, return an error (unlike Redis).
     /// If the value is not/cannot be interpreted as an integer, return an error.
     /// This operation is limited to 64-bit integers.
-    /// Time complexity: O(1)
+    /// If the key does not hold a string, return an error.
     fn decr(&mut self, key: String) -> Result<i64>;
 
     /// Increment the value of a key by a specified amount.
@@ -43,7 +70,7 @@ pub trait Store {
     /// If the key does not exist, return an error (unlike Redis).
     /// If the value is not/cannot be interpreted as an integer, return an error.
     /// This operation is limited to 64-bit integers.
-    /// Time complexity: O(1)
+    /// If the key does not hold a string, return an error.
     fn incrby(&mut self, key: String, delta: i64) -> Result<i64>;
 
     /// Decrement the value of a key by a specifed amount.
@@ -51,7 +78,7 @@ pub trait Store {
     /// If the key does not exist, return an error (unlike Redis).
     /// If the value is not/cannot be interpreted as an integer, return an error.
     /// This operation is limited to 64-bit integers.
-    /// Time complexity: O(1)
+    /// If the key does not hold a string, return an error.
     fn decrby(&mut self, key: String, delta: i64) -> Result<i64>;
 
     // Lists Operations
@@ -59,73 +86,158 @@ pub trait Store {
     /// Insert value at the head of list stored at key.
     /// Return the updated length of the list.
     /// If the key does not exist, create an empty list before performing the operation.
-    /// Time complexity: O(1)
+    /// If the key does not hold a list, return an error.
     fn lpush(&mut self, key: String, val: String) -> Result<u64>;
 
     /// Insert value at the tail of list stored at key.
     /// Return the updated length of the list.
     /// If the key does not exist, create an empty list before performing the operation.
-    /// Time complexity: O(1)
+    /// If the key does not hold a list, return an error.
     fn rpush(&mut self, key: String, val: String) -> Result<u64>;
 
     /// Remove and return the element at the head of list stored at key.
     /// If the list is empty or does not exist, return None.
-    /// Time complexity: O(1)
+    /// If the key does not hold a list, return an error.
     fn lpop(&mut self, key: String) -> Result<Option<String>>;
 
     /// Remove and return the element at the head of list stored at key.
     /// If the list is empty or does not exist, return None.
-    /// Time complexity: O(1)
+    /// If the key does not hold a list, return an error.
     fn rpop(&mut self, key: String) -> Result<Option<String>>;
+
+    /// Return a subarray of the list stored at key. (zero-based index)
+    /// Negative indices refer to the index from the end of the list.
+    /// (ex. -1 refers to last index, -2 refers to second-to-last index, etc.)
+    /// Out-of-range indices do not return errors. Instead, the range is confined
+    /// to the length of the list.
+    /// If the key does not exist, return None.
+    /// If the key does not hold a list, return an error.
+    fn lrange(&self, key: String, start: i64, end: i64) -> Result<Vec<String>>;
+
+    /// Return the element at index of the list stored at key. (zero-based index)
+    /// Negative indices refer to the index from the end of the list.
+    /// (ex. -1 refers to last index, -2 refers to second-to-last index, etc.)
+    /// If the index is out-of-range, return None.
+    /// If the key does not hold a list, return an error.
+    fn lindex(&self, key: String, index: i64) -> Result<Option<String>>;
+
+    /// Insert element into list stored at key before or after pivot value.
+    /// Return the updated length of the list.
+    /// If the key does not exist, return an error.
+    /// If the key does not hold a list, return an error.
+    fn linsert(&mut self, key: String, pivot: String, before: bool) -> Result<u64>;
+
+    /// Return the length of the list stored at key.
+    /// If the key does not exist, return 0.
+    /// If the key does not hold a list, return an error.
+    fn llen(&self, key: String) -> Result<u64>;
 
     // Sets Operations
 
     /// Insert value in the set stored at key.
     /// Return the updated length of the set.
     /// If the key does not exist, create an empty set before performing the operation.
-    /// Time complexity: O(1)
+    /// If the key does not hold a set, return an error.
     fn sadd(&mut self, key: String, val: String) -> Result<u64>;
 
     /// Remove value in the set stored at key.
     /// Return the updated length of the set.
     /// If the key does not exist or the specified value is not a member of the set,
     /// simply ignore and return 0.
-    /// Time complexity: O(1)
+    /// If the key does not hold a set, return an error.
     fn srem(&mut self, key: String, val: String) -> Result<u64>;
 
     /// Return if value is a member of the set stored at key.
     /// If the set is empty or does not exist, return false.
-    /// Time complexity: O(1)
+    /// If the key does not hold a set, return an error.
     fn sismember(&self, key: String, val: String) -> Result<bool>;
 
     /// Return all members of the set stored at key.
     /// If the set is empty or does not exist, return an empty iterator.
-    /// Time complexity: O(N)
+    /// If the key does not hold a set, return an error.
     fn smembers(&self, key: String) -> Result<Vec<String>>;
+
+    /// Return the cardinality of the set stored at key.
+    /// If the key does not exist, return 0.
+    /// If the key does not hold a set, return an error.
+    fn scard(&self, key: String) -> Result<u64>;
+
+    /// Return the intersection of the sets stored at key1 and key2.
+    /// If a key does not exist, assume that the key holds an empty set.
+    /// If a key does not hold a set, return an error.
+    fn sinter(&self, key1: String, key2: String) -> Result<Vec<String>>;
+
+    /// Return the union of the sets stored at key1 and key2.
+    /// If a key does not exist, assume that the key holds an empty set.
+    /// If a key does not hold a set, return an error.
+    fn sunion(&self, key1: String, key2: String) -> Result<Vec<String>>;
+
+    /// Similar to sinter(), but stores the resulting set at dest.
+    /// If dest already exists and holds a set, it is overwritten.
+    /// If dest does not hold a key, return an error.
+    fn sinterstore(&mut self, dest: String, key1: String, key2: String) -> Result<u64>;
 
     // Hashes Operations
 
     /// Get the value related to field in the hash stored at key.
     /// If the key or field does not exist, return None.
-    /// Time complexity: O(1)
+    /// If the key does not hold a hash, return an error.
     fn hget(&self, key: String, field: String) -> Result<Option<String>>;
 
     /// Set the field of the hash stored at key to value.
     /// If the field already existed, return previous value.
     /// Otherwise, return None.
     /// If the key does not exist, create an empty hash before performing the operation.
-    /// Time complexity: O(1)
+    /// If the key does not hold a hash, return an error.
     fn hset(&mut self, key: String, field: String, val: String) -> Result<Option<String>>;
 
     /// Remove field from the hash stored at key.
     /// Return the number of fields that were deleted.
     /// If the key or field does not exist, do nothing (and return 0).
-    /// Time complexity: O(1)
+    /// If the key does not hold a has, return an error.
     fn hdel(&mut self, key: String, field: String) -> Result<u64>;
+
+    /// Increment the value of field in a hash stored at key, by a specified amount.
+    /// Return the updated value.
+    /// This operation is limited to 64-bit integers.
+    /// If the key or field does not exist, return an error (unlike Redis).
+    /// If the value is not/cannot be interpreted as an integer, return an error.
+    /// If the key does not hold a hash, return an error.
+    fn hincrby(&mut self, key: String, field: String, delta: i64) -> Result<i64>;
+
+    /// Return the number of fields contained in the hash stored at key.
+    /// If the key does not exist, return 0.
+    /// If the key does not hold a hash, return an error.
+    fn hlen(&self, key: String) -> Result<u64>;
+
+    /// Return the length of the string stored at key.
+    /// If the key or field does not exist, return 0.
+    /// If the key does not hold a string, return an error.
+    fn hstrlen(&self, key: String, field: String) -> Result<u64>;
+
+    /// Return all fields and values in the hash stored at key.
+    /// If the key does not exist, return an empty vector.
+    /// If the key does not store a hash, return an error.
+    fn hgetall(&self, key: String) -> Result<Vec<String>>;
+
+    /// Return all values in the hash stored at key.
+    /// If the key does not exist, return an empty vector.
+    /// If the key does not store a hash, return an error.
+    fn hvals(&self, key: String) -> Result<Vec<String>>;
+}
+
+#[derive(Debug)]
+enum DataType {
+    Null,
+    StringType,
+    ListType,
+    HashType,
+    SetType,
 }
 
 #[derive(Debug)]
 pub struct StdStore {
+    namespace: HashMap<String, DataType>,
     strings: HashMap<String, String>,
     lists: HashMap<String, VecDeque<String>>,
     hashes: HashMap<String, HashMap<String, String>>,
@@ -172,6 +284,7 @@ impl StdStore {
 impl Store for StdStore {
     fn new() -> Self {
         StdStore {
+            namespace: HashMap::new(),
             strings: HashMap::new(),
             lists: HashMap::new(),
             hashes: HashMap::new(),
